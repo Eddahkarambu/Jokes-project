018b5bcbd70a41d02f04b18468333ee1f5630c20
Eddahkarambu <gikunaeddah@gmail.com>
2021-10-18 16:01:21 +0300

    added a list of categories

--- src/components/Jokes.js
+++ src/components/Jokes.js
@@ -1,12 +1,54 @@
-import React,{useState} from 'react'
+import React,{useEffect, useState} from 'react'
 import axios from 'axios';
 
 const Joke=()=>{
     const [joke, setJoke]= useState("");
+    const [categories, setCategories]= useState[""];
+
+
+    
+       const fetchcategoryList =()=>{
+        axios.get('https://api.chucknorris.io/jokes/categories')
+        .then((response) =>{
+            const list=response.data
+            setCategories(list)
+
+        })
+     }
+
+     useEffect(() =>{
+       fetchcategoryList ()
+     }, [])
+    
 
     const fetchJoke=()=>{
         axios.get('https://api.chucknorris.io/jokes/random')
         .then((response) =>{
-            const {Joke}
+            const {value}= response.data
+            setJoke(value)
         })
     }
+
+
+    return(
+        <div className="app">
+            <div className="card">
+                <div>
+                    {categories.map((category, index)=>(
+                        <list key={index}>{category}</list>
+
+                    ))}
+                </div>
+                <h1 className="heading">{joke}</h1>
+                <button className= "button" onClick={fetchJoke}>
+                    <span> GIVE ME A JOKE</span>
+
+                </button>
+
+            </div>
+
+        </div>
+    );
+}
+
+export default Joke;
